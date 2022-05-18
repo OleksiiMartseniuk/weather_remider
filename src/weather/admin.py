58e7de9e3f010c32340f222849a78a4b89f28663
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-18 14:54:10 +0300

    add register SubscriptionCity

--- src/weather/admin.py
+++ src/weather/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from src.weather.models import City
+from src.weather.models import City, SubscriptionCity
 
 
 @admin.register(City)
 class CitiAdmin(admin.ModelAdmin):
     list_display = ('name', 'id_city',)
     list_filter = ('name', 'id_city',)
+
+
+@admin.register(SubscriptionCity)
+class SubscriptionCityAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'city', 'periodicity_send_email')
+    list_filter = ('owner', 'city')
