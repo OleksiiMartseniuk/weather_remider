3c3af694eede39edcea77f780eb6644a420fa079
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-18 12:42:31 +0300

    add SECRET_KEY_OPEN_WEATHER_MAP

--- config/settings.py
+++ config/settings.py
@@ -114,3 +114,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECRET_KEY_OPEN_WEATHER_MAP = os.getenv('SECRET_KEY_OPEN_WEATHER_MAP')
