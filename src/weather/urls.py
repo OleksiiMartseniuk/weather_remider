4892fcb0e47a6b2caff17198b338719bd2c984ef
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-19 23:16:19 +0300

    add path subscription

--- src/weather/urls.py
+++ src/weather/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from src.weather.views import CityView
+from src.weather import views
 
 urlpatterns = [
-    path('city/<str:name>/', CityView.as_view({'get': 'retrieve'})),
-    path('city/', CityView.as_view({'get': 'list', 'post': 'create'})),
+    path('city/<str:name>/', views.CityView.as_view({'get': 'retrieve'})),
+    path('city/', views.CityView.as_view({'get': 'list', 'post': 'create'})),
+    path('subscription/<int:pk>/', views.SubscriptionCityView.as_view({'get': 'retrieve',
+                                                                       'put': 'update',
+                                                                       'delete': 'destroy'})),
+    path('subscription/', views.SubscriptionCityView.as_view({'get': 'list', 'post': 'create'})),
 ]
