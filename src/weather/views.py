4e3f05c8d9e9546b5ed8be9fefa42b3edb25dc7f
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-19 23:16:05 +0300

    add SubscriptionCityView

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from src.weather import serializers
 from src.weather.service.service import weather_s
@@ -17,5 +18,19 @@ class CityView(viewsets.ModelViewSet):
         else:
             return self.serializer_class
 
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
     def perform_create(self, serializer):
         weather_s.create(serializer.validated_data['name'])
+
+
+class SubscriptionCityView(viewsets.ModelViewSet):
+    """ Подписки """
+    serializer_class = serializers.SubscriptionCitySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return models.SubscriptionCity.objects.filter(owner=self.request.user)
