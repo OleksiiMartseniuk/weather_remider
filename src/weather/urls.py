5d1339afa45d2c2d51d5c77b199fc587b2067c64
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-19 19:17:29 +0300

    add path

--- src/weather/urls.py
+++ src/weather/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from src.weather.views import CityView
 
 urlpatterns = [
-
+    path('city/<str:name>/', CityView.as_view({'get': 'retrieve'})),
+    path('city/', CityView.as_view({'get': 'list', 'post': 'create'})),
 ]
