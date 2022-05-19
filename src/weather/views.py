11180ee29a74e73f265fc47eefad4eb169684a60
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-19 19:16:19 +0300

    create CityView

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from src.weather import serializers
+from src.weather.service.service import weather_s
+from src.weather import models
+
+
+class CityView(viewsets.ModelViewSet):
+    """ Город """
+    queryset = models.City.objects.all()
+    serializer_class = serializers.CitySerializer
+    lookup_field = 'name'
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return serializers.CityCreateSerializer
+        else:
+            return self.serializer_class
+
+    def perform_create(self, serializer):
+        weather_s.create(serializer.validated_data['name'])
