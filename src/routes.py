285946b3f27506d422cbc0fc336590922397e8fd
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-19 21:35:40 +0300

    add rest_framework_simplejwt

--- src/routes.py
+++ src/routes.py
@@ -3,6 +3,11 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,6 +27,9 @@ urlpatterns = [
     # drf_yasg
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    # Simple JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # app weather
     path('api/weather/', include('src.weather.urls')),
 ]
