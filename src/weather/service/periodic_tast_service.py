3bf1e066332382cb5f528a99db2fc90d1ec98c41
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-22 21:29:36 +0300

    return task

--- src/weather/service/periodic_tast_service.py
+++ src/weather/service/periodic_tast_service.py
@@ -2,18 +2,22 @@ from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 
 class ServiceTasks:
-    def get_schedule(self, time: int):
+    def get_schedule(self, time: int) -> None:
         return IntervalSchedule.objects.filter(every=time, period=IntervalSchedule.HOURS)[0]
 
-    def create(self, time: int, name: str):
+    def create(self, time: int, name: str, city_id: int) -> PeriodicTask:
         schedule = self.get_schedule(time)
-        PeriodicTask.objects.create(
+        task = PeriodicTask.objects.create(
             interval=schedule,
             name=name,
             task='src.weather.tasks.sent_weather_email'
         )
+        return task
 
-    def update(self, time: int, task: PeriodicTask):
+    def update(self, time: int, task: PeriodicTask) -> None:
         schedule = self.get_schedule(time)
         task.interval = schedule
         task.save()
+
+
+service_task = ServiceTasks()
