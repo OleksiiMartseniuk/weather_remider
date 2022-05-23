56f68da96f35000b41155e20819d904108e4faf4
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-23 12:39:02 +0300

    add sent_weather_email

--- src/base/celery_create_time.py
+++ src/base/celery_create_time.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 
 def create_interval_schedule():
+    """ Создания Интервального расписания"""
     for time in settings.TIME_LIST:
         IntervalSchedule.objects.get_or_create(
             every=time,
--- src/weather/tasks.py
+++ src/weather/tasks.py
@@ -1,11 +1,15 @@
 from celery import shared_task
 from time import sleep
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
 from src.weather.models import City
 from src.base.openweathermaplib import OpenWeatherMapClient
 
 
 @shared_task
 def update_city_data():
+    """ Обновления данных всех городов """
     client = OpenWeatherMapClient()
 
     for position, city in enumerate(City.objects.all(), 1):
@@ -40,3 +44,15 @@ def update_city_data():
             city.sys_sunrise = data_city['sys']['sunrise']
             city.sys_sunset = data_city['sys']['sunset']
             city.save()
+
+
+@shared_task
+def sent_weather_email(city_id: int, owner_email: str):
+    """ Отправка email пользователю """
+    city = City.objects.get(id=city_id)
+    message = render_to_string('weather/weather_email.html', {'city': city})
+    send_mail(subject='Weather',
+              html_message=message,
+              message=f'Weather in {city.name}',
+              from_email=settings.EMAIL_HOST_USER,
+              recipient_list=[owner_email])
