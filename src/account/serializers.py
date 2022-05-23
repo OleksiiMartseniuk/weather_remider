87fe3fd6a7130b1ffe795bb5080fa1d7d491c937
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-23 14:31:30 +0300

    create UserView

---
+++ src/account/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=get_user_model().objects.all())]
+    )
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
