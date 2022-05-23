62539096be348073bd908eb8a63ba33916c3dd9f
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-23 14:31:13 +0300

    add UserView

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from src.account import serializers
 
-# Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = serializers.UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
+
+    def get_object(self):
+        return self.request.user
