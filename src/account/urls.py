c86c3c4442fe5812cd9eef66c1af79b292237eb9
martsenyk.l@gmail.com <6n8at3TUS7>
2022-05-24 15:55:59 +0300

    app path

--- src/account/urls.py
+++ src/account/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from src.account import views
 
 urlpatterns = [
-    path('create/', views.UserView.as_view({'post': 'create', 'get': 'retrieve'})),
+    path('create/', views.UserView.as_view({'post': 'create',
+                                            'get': 'retrieve',
+                                            'put': 'update',
+                                            'delete': 'destroy'})),
 ]
