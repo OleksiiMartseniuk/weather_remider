f59d20f807a948e97d82d3ec22bea698c5a56868
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-07 22:28:49 +0300

    add method search_city and create return[int]

--- src/weather/service/service.py
+++ src/weather/service/service.py
@@ -1,15 +1,23 @@
 from src.base.openweathermaplib import OpenWeatherMapClient
 from src.weather.service.write_db import WriteDB
+from src.weather.models import City
 
 
 class WeatherService:
     writer = WriteDB()
     client = OpenWeatherMapClient()
 
-    def create(self, city_name: str) -> None:
+    def create(self, city_name: str) -> int:
         """ Создать город """
         data_city = self.client.get_weather(city_name=city_name)
-        self.writer.create_city(data_city)
+        return self.writer.create_city(data_city)
+
+    def search_city(self, name: str) -> int:
+        """Получения города"""
+        if City.objects.filter(name=name.lower()).exists():
+            city = City.objects.get(name=name.lower())
+            return city.id
+        return self.create(name)
 
 
 weather_s = WeatherService()
