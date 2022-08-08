9e4126d496fca2010f72d86237b75182ff4d7c6d
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-08 11:56:31 +0300

    remove permission_classes

--- src/account/utils.py
+++ src/account/utils.py
@@ -1,9 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
-
-
 class UserMixin:
-
-    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         """Получения текущего пользователя"""
